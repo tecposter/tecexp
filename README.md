29c599fc901a4509214d18e84ecbf1ee17424335
zjh@sun <zhanjh@126.com>
2024-07-13 18:11:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# TecExp: Tec Markdown Exporter
+
+`tecexp` export Markdown documents from Obsidian to Hugo. `tecexp` find documents with property `public: web` in Obsidian vault directory and export them to Hugo posts directory.
+
+## Install
+
+Clone the repository and use `cargo-install` to install
+
+```shell
+git clone https://github.com/tecposter/tecexp.git
+cd tecexp
+cargo install --path .
+```
+
+If rust is installed by asdf, run the following command to recreate shims
+
+```shell
+asdf reshim rust
+```
+
+## Usage
+
+```shell
+❯ tecexp -h
+Export mds from Obsidian to Hugo
+
+Usage: tecexp [OPTIONS] --obsidian-dir <OBSIDIAN_DIR> --hugo-dir <HUGO_DIR>
+
+Options:
+  -o, --obsidian-dir <OBSIDIAN_DIR>        Obsidian vault dir
+  -g, --hugo-dir <HUGO_DIR>                Hugo dir
+  -p, --hugo-posts-dir <HUGO_POSTS_DIR>    Hugo posts sub dir [default: content/posts]
+  -a, --hugo-assets-dir <HUGO_ASSETS_DIR>  Hugo assets sub dir [default: content/assets]
+  -w, --watch                              Watch
+  -h, --help                               Print help
+  -V, --version                            Print version
+```
---
+++ src/main.rs
@@ -0,0 +1,350 @@
+use std::{
+  collections::HashMap,
+  ffi::OsStr,
+  fs::{self, File},
+  io::{BufRead, BufReader, BufWriter, Lines, Write},
+  iter::{Flatten, Peekable},
+  path::Path,
+  sync::mpsc::channel,
+};
+
+use anyhow::Result;
+use clap::Parser;
+use notify::{Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use time::{format_description::well_known::Iso8601, OffsetDateTime};
+
+#[derive(Debug, Clone)]
+enum Prop {
+  Str(String),
+  Vec(Vec<String>),
+}
+
+/// Export mds from Obsidian to Hugo
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+  /// Obsidian vault dir
+  #[arg(short, long)]
+  obsidian_dir: String,
+
+  /// Hugo dir
+  #[arg(short('g'), long)]
+  hugo_dir: String,
+
+  /// Hugo posts sub dir
+  #[arg(short('p'), long, default_value = "content/posts")]
+  hugo_posts_dir: String,
+
+  /// Hugo assets sub dir
+  #[arg(short('a'), long, default_value = "content/assets")]
+  hugo_assets_dir: String,
+
+  /// Watch
+  #[arg(short, long, default_value_t = false)]
+  watch: bool,
+}
+
+fn main() -> Result<()> {
+  let args = Args::parse();
+
+  let obsidian_dir = fs::canonicalize(args.obsidian_dir).expect("Cannot find Obsidian vault dir");
+  let hugo_dir = fs::canonicalize(args.hugo_dir).expect("Cannot find hugo dir");
+
+  let src_dir = obsidian_dir;
+  let asset_src = src_dir.join("assets");
+
+  let dst_dir = hugo_dir.join(args.hugo_posts_dir);
+  let asset_dst = hugo_dir.join(args.hugo_assets_dir);
+
+  if dst_dir.exists() {
+    fs::remove_dir_all(&dst_dir)?;
+  }
+  fs::create_dir(&dst_dir)?;
+  if asset_dst.exists() {
+    fs::remove_dir_all(&asset_dst)?;
+  }
+  fs::create_dir(&asset_dst)?;
+
+  recursive_scan(&src_dir, Path::new(""), &|sub_path| {
+    export(
+      &src_dir.join(sub_path),
+      &dst_dir.join(to_url(sub_path.to_str().unwrap())),
+      &asset_src,
+      &asset_dst,
+    )
+  })?;
+
+  if !args.watch {
+    return Ok(());
+  }
+
+  let (tx, rx) = channel();
+  let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+  watcher.watch(&src_dir, RecursiveMode::Recursive)?;
+
+  for res in rx {
+    match res {
+      Ok(event) => match event.kind {
+        EventKind::Modify(_) => {
+          for full_path in &event.paths {
+            let file_name = full_path.file_name().unwrap().to_str().unwrap();
+            if file_name.starts_with('.') || file_name.ends_with('~') {
+              continue;
+            }
+            if let Ok(sub_path) = full_path.strip_prefix(&src_dir) {
+              export(
+                &full_path,
+                &dst_dir.join(to_url(sub_path.to_str().unwrap())),
+                &asset_src,
+                &asset_dst,
+              )?;
+            }
+          }
+        }
+        _ => {}
+      },
+      Err(error) => println!("Error: {error:?}"),
+    }
+  }
+
+  Ok(())
+}
+
+fn recursive_scan(base_dir: &Path, sub_dir: &Path, cb: &dyn Fn(&Path) -> Result<()>) -> Result<()> {
+  let dir = base_dir.join(sub_dir);
+
+  if dir.is_dir() {
+    for res in fs::read_dir(&dir)? {
+      let entry = res?;
+      let path = entry.path();
+      let name = entry.file_name();
+      if name.as_encoded_bytes()[0] == b'.' {
+        continue;
+      }
+      let sub_path = sub_dir.join(name);
+      if path.is_dir() {
+        recursive_scan(base_dir, &sub_path, cb)?;
+      } else {
+        if Some(OsStr::new("md")) == path.extension() {
+          // println!("{sub_path:?}");
+          cb(&sub_path)?;
+        }
+      }
+    }
+  }
+  Ok(())
+}
+
+// fn to_hex_path(path: &Path) -> String {
+//   let bytes = path.as_os_str().as_encoded_bytes();
+//   let mut p = bytes[..bytes.len() - 3]
+//     .iter()
+//     .map(|b| format!("{:02x}", b))
+//     .collect::<Vec<String>>()
+//     .join("");
+//   p.push_str(".md");
+//   p
+// }
+
+fn to_url(p: &str) -> String {
+  p.replace(" ", "-").replace("/", "-").to_lowercase()
+  // let p = path
+  //   .to_str()
+  //   .unwrap()
+  //   .replace(" ", "-")
+  //   .replace("/", "-")
+  //   .to_lowercase();
+  // p
+  // form_urlencoded::byte_serialize(p.as_bytes()).collect()
+  // let bytes: Vec<u8> = path
+  //   .as_os_str()
+  //   .to_ascii_lowercase()
+  //   .as_encoded_bytes()
+  //   .iter()
+  //   .map(|b| if b.is_ascii_whitespace() { b'-' } else { *b })
+  //   .collect();
+  // String::from_utf8_lossy(&bytes).to_string()
+}
+
+fn export(src: &Path, dst: &Path, asset_src: &Path, asset_dst: &Path) -> Result<()> {
+  let src_file = File::open(src)?;
+  let mut src_lines = BufReader::new(src_file).lines().flatten().peekable();
+  if let Some(src_props) = extract_src_props(&mut src_lines) {
+    if !contain_publish_web(&src_props) {
+      return Ok(());
+    }
+
+    if !is_modified(src, dst) {
+      return Ok(());
+    }
+
+    println!("export: {src:?} \n    -> {dst:?} \n");
+
+    let dst_props = build_dst_props(&src_props, src);
+
+    let dst_file = File::create(dst)?;
+    let mut writer = BufWriter::new(dst_file);
+    writeln!(writer, "---")?;
+    for (key, val) in dst_props.iter() {
+      match val {
+        Prop::Str(s) => {
+          writeln!(writer, "{key}: {s}")?;
+        }
+        Prop::Vec(v) => {
+          writeln!(writer, "{key}:")?;
+          for item in v {
+            writeln!(writer, " - {item}")?;
+          }
+        }
+      }
+    }
+    writeln!(writer, "---")?;
+
+    for line in src_lines {
+      let mut curr = 0;
+      while let Some(start) = line[curr..].find("[[") {
+        write!(writer, "{}", &line[curr..(curr + start)])?;
+        curr += start;
+        if let Some(end) = line[(curr + 2)..].find("]]") {
+          let inner = &line[(curr + 2)..(curr + 2 + end)];
+          if inner.ends_with(".png") || inner.ends_with(".jpg") {
+            // let img_src = asset_src.join(inner);
+            // let img_dst = asset_dst.join(inner);
+            // println!("{img_src:?} -> {img_dst:?}");
+            fs::copy(asset_src.join(inner), asset_dst.join(inner))?;
+            write!(writer, "[{inner}](/assets/{inner})")?;
+          } else {
+            write!(writer, "[{}](/posts/{}/)", inner, to_url(inner))?;
+          }
+          curr += 2 + end + 2;
+        }
+      }
+      write!(writer, "{}\n", &line[curr..])?;
+    }
+    writer.flush()?;
+  }
+
+  Ok(())
+}
+
+fn build_dst_props(src_props: &HashMap<String, Prop>, src: &Path) -> HashMap<String, Prop> {
+  let mut props: HashMap<String, Prop> = HashMap::new();
+
+  let title = src
+    .file_name()
+    .unwrap()
+    .to_str()
+    .unwrap()
+    .trim_end_matches(".md");
+
+  props.insert("title".to_string(), Prop::Str(title.to_string()));
+
+  let modified: OffsetDateTime = fs::metadata(src).unwrap().modified().unwrap().into();
+  props.insert(
+    "date".to_string(),
+    Prop::Str(modified.format(&Iso8601::DEFAULT).unwrap()),
+  );
+
+  if let Some(tags) = src_props.get("tags") {
+    props.insert("tags".to_string(), tags.clone());
+  }
+
+  props
+}
+
+fn contain_publish_web(props: &HashMap<String, Prop>) -> bool {
+  if let Some(Prop::Str(v)) = props.get("publish") {
+    v.eq("web")
+  } else {
+    false
+  }
+}
+
+fn is_modified(src: &Path, dst: &Path) -> bool {
+  if !dst.exists() {
+    true
+  } else {
+    let src_modified = fs::metadata(src).unwrap().modified().unwrap();
+    let dst_modified = fs::metadata(dst).unwrap().modified().unwrap();
+    src_modified.gt(&dst_modified)
+  }
+}
+
+fn extract_src_props(
+  lines: &mut Peekable<Flatten<Lines<BufReader<File>>>>,
+) -> Option<HashMap<String, Prop>> {
+  while let Some(line) = lines.peek() {
+    if line.is_empty() {
+      lines.next();
+    } else {
+      break;
+    }
+  }
+  if let Some(line) = lines.peek() {
+    if line.trim().eq("---") {
+      lines.next();
+    } else {
+      return None;
+    }
+  }
+
+  let mut props: HashMap<String, Prop> = HashMap::new();
+  let mut vec_key = String::new();
+
+  while let Some(line) = lines.next() {
+    if line.trim().eq("---") {
+      break;
+    }
+    // println!("> {line}");
+    if let Some(pos) = line.find(':') {
+      let key = line[..pos].trim();
+      let val = line[(pos + 1)..].trim();
+      if !key.is_empty() && !val.is_empty() {
+        if let Some(vec) = str_to_vec(val) {
+          props.insert(key.to_string(), Prop::Vec(vec));
+        } else {
+          props.insert(key.to_string(), Prop::Str(val.to_string()));
+        }
+        vec_key = "".to_string();
+      } else if !key.is_empty() && val.is_empty() {
+        vec_key = key.to_string();
+        props.insert(key.to_string(), Prop::Vec(vec![]));
+      } else {
+        vec_key = "".to_string();
+      }
+    } else if let Some(pos) = line.find('-') {
+      let pre = line[..pos].trim();
+      if !pre.is_empty() {
+        continue;
+      }
+      if vec_key.is_empty() {
+        continue;
+      }
+      let val = line[(pos + 1)..].trim();
+      if val.is_empty() {
+        continue;
+      }
+
+      if let Some(Prop::Vec(vec)) = props.get_mut(&vec_key) {
+        vec.push(val.to_string());
+      }
+    }
+  }
+  if props.len() > 0 {
+    Some(props)
+  } else {
+    None
+  }
+}
+
+fn str_to_vec(val: &str) -> Option<Vec<String>> {
+  if val.starts_with('[') && val.ends_with(']') {
+    let items = val[1..val.len() - 1]
+      .split(',')
+      .map(|item| item.trim().trim_matches('"').to_string())
+      .collect();
+    Some(items)
+  } else {
+    None
+  }
+}
